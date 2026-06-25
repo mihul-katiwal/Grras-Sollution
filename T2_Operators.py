59bc21c47e8d0f8757f950fc57dee8db36ead4b1
mihul-katiwal <mihul9116640022@gmail.com>
2026-06-25 11:53:35 +0530

    Update T2_Operators.py

--- T2_Operators.py
+++ T2_Operators.py
@@ -2,44 +2,44 @@
 # +, -, *, /, //, %, **
 
 # +
-# a = 10
-# b = 20
-# print(a + b)
+a = 10
+b = 20
+print(a + b)
 
 # -
-# c = 20
-# d = 10
-# print(c - d)
+c = 20
+d = 10
+print(c - d)
 
 # *
-# e = 10
-# f = 5
-# print(e * f)
+e = 10
+f = 5
+print(e * f)
 
 # /
-# num1 = 20
-# num2 = 2
-# print(num1 / num2)
+num1 = 20
+num2 = 2
+print(num1 / num2)
 
 # //
-# num3 = 20
-# num4 = 2
-# print(num3 // num4)
+num3 = 20
+num4 = 2
+print(num3 // num4)
 
 # %
-# num5 = 10
-# num6 = 3
-# print(num5 % num6)
+num5 = 10
+num6 = 3
+print(num5 % num6)
 
 # **
-# num7 = 5
-# num8 = 3
-# print(num7 ** num8)
+num7 = 5
+num8 = 3
+print(num7 ** num8)
 
 # Example (even or odd)
-# num1 = int(input("Enter first number: "))
-# num2 = 2
-# print(num1 % num2)
+num1 = int(input("Enter first number: "))
+num2 = 2
+print(num1 % num2)
 
 
 
@@ -47,34 +47,34 @@
 # ==, !=, >, <, >=, <=
 
 # ==
-# num1 = 10
-# num2 = 11
-# print(num1 == num2)
+num1 = 10
+num2 = 11
+print(num1 == num2)
 
 # <
-# num1 = 10
-# num2 = 11
-# print(num1 < num2)
+num1 = 10
+num2 = 11
+print(num1 < num2)
 
 # >
-# num1 = 20
-# num2 = 10
-# print(num1 > num2)
+num1 = 20
+num2 = 10
+print(num1 > num2)
 
 # <=
-# num1 = 10
-# num2 = 10
-# print(num1 <= num2)
+num1 = 10
+num2 = 10
+print(num1 <= num2)
 
 # >=
-# num1 = 20   
-# num2 = 10
-# print(num1 >= num2)
+num1 = 20   
+num2 = 10
+print(num1 >= num2)
 
 # !=
-# num1 = 10
-# num2 = 10
-# print(num1 != num2)
+num1 = 10
+num2 = 10
+print(num1 != num2)
 
 
 
@@ -82,44 +82,44 @@
 # =, +=, -=, *=, /=, //=, %=, **=
 
 # =
-# a = 10
-# b = a + 20
-# print(b)
+a = 10
+b = a + 20
+print(b)
 
 # +=
-# a = 10
-# a += 20
-# print(a)
+a = 10
+a += 20
+print(a)
 
 # -=
-# a = 20
-# a -= 10
-# print(a)
+a = 20
+a -= 10
+print(a)
 
 # *=
-# a = 10
-# a *= 5
-# print(a)
+a = 10
+a *= 5
+print(a)
 
 # /=
-# a = 20
-# a /= 2
-# print(a)
+a = 20
+a /= 2
+print(a)
 
 # //=
-# a = 20
-# a //= 2
-# print(a)
+a = 20
+a //= 2
+print(a)
 
 # %=
-# a = 10
-# a %= 3
-# print(a)
+a = 10
+a %= 3
+print(a)
 
 # **=
-# a = 5
-# a **= 3
-# print(a)
+a = 5
+a **= 3
+print(a)
 
 
 
@@ -127,28 +127,28 @@
 # and, or, not
 
 # and
-# a = 10
-# b = 20
-# print(a and b)
+a = 10
+b = 20
+print(a and b)
 
 # or
-# a = 10
-# b = 20
-# print(a or b)
+a = 10
+b = 20
+print(a or b)
 
 # not
-# num = []
-# print(not num)
+num = []
+print(not num)
 
-# a = -10
-# b = 10
-# print(a and b)
-# print(a or b)
+a = -10
+b = 10
+print(a and b)
+print(a or b)
 
-# name1 = "Mihul"
-# name2 = ""
-# print(name1 and name2)
-# print(name1 or name2)
+name1 = "Mihul"
+name2 = ""
+print(name1 and name2)
+print(name1 or name2)
 
 
 
@@ -156,35 +156,35 @@
 # &, |, ^, ~, <<, >>
 
 # &
-# a = 5
-# b = 2
-# print(a & b)
+a = 5
+b = 2
+print(a & b)
 
 # |
-# a = 5
-# b = 2
-# print(a | b)
+a = 5
+b = 2
+print(a | b)
 
 # ^
-# a = 5
-# b = 2
-# print(a ^ b)
+a = 5
+b = 2
+print(a ^ b)
 
 # ~
-# a = 4
-# print(~a)
+a = 4
+print(~a)
 
 # <<
-# a = 5
-# print(a << 1)
+a = 5
+print(a << 1)
 
 # >>
-# a = 5
-# print(a >> 1)
+a = 5
+print(a >> 1)
 
 # example
-# name = 10
-# name1 = 10
-# print(name == name1)
-# print(name is name1)
-# print(id(name), id(name1))
+name = 10
+name1 = 10
+print(name == name1)
+print(name is name1)
+print(id(name), id(name1))
