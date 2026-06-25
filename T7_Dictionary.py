f6347f86b8609a43f16fcc5adfd0bace1eb7d455
mihul-katiwal <mihul9116640022@gmail.com>
2026-06-25 11:59:12 +0530

    Update T7_Dictionary.py

--- T7_Dictionary.py
+++ T7_Dictionary.py
@@ -1,139 +1,139 @@
 # dict
-# d = {"name":"hello","age":20}
+d = {"name":"hello","age":20}
 
-# print(d.keys())
-# print(d.values())
+print(d.keys())
+print(d.values())
 
-# for i in d.keys():
-#   print("key=",i)
-#   print("value=",d[i])
-
-
-
-# d = {
-#   "Message": "Number of Post office(s) found: 6",
-#   "Status": "Success",
-#   "PostOffice": [
-#     {
-#       "Name": "Achrol",
-#       "Description": "",
-#       "BranchType": "Sub Post Office",
-#       "DeliveryStatus": "Delivery",
-#       "Taluk": "Jaipur",
-#       "Circle": "Jaipur",
-#       "District": "Jaipur",
-#       "Division": "Jaipur Moffusil",
-#       "Region": "Jaipur HQ",
-#       "State": "Rajasthan",
-#       "Country": "India"
-#     },
-#     {
-#       "Name": "Jaitpura Khinchi",
-#       "Description": "",
-#       "BranchType": "Branch Post Office",
-#       "DeliveryStatus": "Delivery",
-#       "Taluk": "Jaipur",
-#       "Circle": "Jaipur",
-#       "District": "Jaipur",
-#       "Division": "Jaipur Moffusil",
-#       "Region": "Jaipur HQ",
-#       "State": "Rajasthan",
-#       "Country": "India"
-#     },
-#     {
-#       "Name": "Kali Pahadi",
-#       "Description": "",
-#       "BranchType": "Branch Post Office",
-#       "DeliveryStatus": "Delivery",
-#       "Taluk": "Jaipur",
-#       "Circle": "Jaipur",
-#       "District": "Jaipur",
-#       "Division": "Jaipur Moffusil",
-#       "Region": "Jaipur HQ",
-#       "State": "Rajasthan",
-#       "Country": "India"
-#     },
-#     {
-#       "Name": "Kant",
-#       "Description": "",
-#       "BranchType": "Branch Post Office",
-#       "DeliveryStatus": "Delivery",
-#       "Taluk": "Jaipur",
-#       "Circle": "Jaipur",
-#       "District": "Jaipur",
-#       "Division": "Jaipur Moffusil",
-#       "Region": "Jaipur HQ",
-#       "State": "Rajasthan",
-#       "Country": "India"
-#     },
-#     {
-#       "Name": "Labana",
-#       "Description": "",
-#       "BranchType": "Branch Post Office",
-#       "DeliveryStatus": "Delivery",
-#       "Taluk": "Jaipur",
-#       "Circle": "Jaipur",
-#       "District": "Jaipur",
-#       "Division": "Jaipur Moffusil",
-#       "Region": "Jaipur HQ",
-#       "State": "Rajasthan",
-#       "Country": "India"
-#     },
-#     {
-#       "Name": "Rajpurawas Tala",
-#       "Description": "",
-#       "BranchType": "Branch Post Office",
-#       "DeliveryStatus": "Delivery",
-#       "Taluk": "Jaipur",
-#       "Circle": "Jaipur",
-#       "District": "Jaipur",
-#       "Division": "Jaipur Moffusil",
-#       "Region": "Jaipur HQ",
-#       "State": "Rajasthan",
-#       "Country": "India"
-#     }
-#   ]
-# }
-# print(d["Message"])
-# print(d["Status"])
-# for i in d["PostOffice"]:
-#   print(i["Name"])
-#   print(i["Taluk"])
-#   print(i["Circle"])
-#   print(i["District"])
-#   print(i["Division"])
-#   print(i["Region"])
-#   print(i["State"])
-#   print(i["Country"])
-#   print("\n")
+for i in d.keys():
+  print("key=",i)
+  print("value=",d[i])
 
 
-# D = {"name":"hello","age":20}
-# D.update({"name": "arya mains"})
-# D['name'] = "arya mains new"
-# print(D["age"])
-# print(D)
 
-# L = [20,30]
-# print(L[0])
+d = {
+  "Message": "Number of Post office(s) found: 6",
+  "Status": "Success",
+  "PostOffice": [
+    {
+      "Name": "Achrol",
+      "Description": "",
+      "BranchType": "Sub Post Office",
+      "DeliveryStatus": "Delivery",
+      "Taluk": "Jaipur",
+      "Circle": "Jaipur",
+      "District": "Jaipur",
+      "Division": "Jaipur Moffusil",
+      "Region": "Jaipur HQ",
+      "State": "Rajasthan",
+      "Country": "India"
+    },
+    {
+      "Name": "Jaitpura Khinchi",
+      "Description": "",
+      "BranchType": "Branch Post Office",
+      "DeliveryStatus": "Delivery",
+      "Taluk": "Jaipur",
+      "Circle": "Jaipur",
+      "District": "Jaipur",
+      "Division": "Jaipur Moffusil",
+      "Region": "Jaipur HQ",
+      "State": "Rajasthan",
+      "Country": "India"
+    },
+    {
+      "Name": "Kali Pahadi",
+      "Description": "",
+      "BranchType": "Branch Post Office",
+      "DeliveryStatus": "Delivery",
+      "Taluk": "Jaipur",
+      "Circle": "Jaipur",
+      "District": "Jaipur",
+      "Division": "Jaipur Moffusil",
+      "Region": "Jaipur HQ",
+      "State": "Rajasthan",
+      "Country": "India"
+    },
+    {
+      "Name": "Kant",
+      "Description": "",
+      "BranchType": "Branch Post Office",
+      "DeliveryStatus": "Delivery",
+      "Taluk": "Jaipur",
+      "Circle": "Jaipur",
+      "District": "Jaipur",
+      "Division": "Jaipur Moffusil",
+      "Region": "Jaipur HQ",
+      "State": "Rajasthan",
+      "Country": "India"
+    },
+    {
+      "Name": "Labana",
+      "Description": "",
+      "BranchType": "Branch Post Office",
+      "DeliveryStatus": "Delivery",
+      "Taluk": "Jaipur",
+      "Circle": "Jaipur",
+      "District": "Jaipur",
+      "Division": "Jaipur Moffusil",
+      "Region": "Jaipur HQ",
+      "State": "Rajasthan",
+      "Country": "India"
+    },
+    {
+      "Name": "Rajpurawas Tala",
+      "Description": "",
+      "BranchType": "Branch Post Office",
+      "DeliveryStatus": "Delivery",
+      "Taluk": "Jaipur",
+      "Circle": "Jaipur",
+      "District": "Jaipur",
+      "Division": "Jaipur Moffusil",
+      "Region": "Jaipur HQ",
+      "State": "Rajasthan",
+      "Country": "India"
+    }
+  ]
+}
+print(d["Message"])
+print(d["Status"])
+for i in d["PostOffice"]:
+  print(i["Name"])
+  print(i["Taluk"])
+  print(i["Circle"])
+  print(i["District"])
+  print(i["Division"])
+  print(i["Region"])
+  print(i["State"])
+  print(i["Country"])
+  print("\n")
 
 
-# D = {"name":"hello","age":20}
-# D.update({"name": "arya mains"})
-# D['name'] = "arya mains new"
-# del D["name"]
-# print(D)
+D = {"name":"hello","age":20}
+D.update({"name": "arya mains"})
+D['name'] = "arya mains new"
+print(D["age"])
+print(D)
+
+L = [20,30]
+print(L[0])
+
+
+D = {"name":"hello","age":20}
+D.update({"name": "arya mains"})
+D['name'] = "arya mains new"
+del D["name"]
+print(D)
 
 
 # nested dict
 
-# D = {"address":{"address1":{"city":"kukas city","district":"jaipur"},
-#                 "address2":{"city":"gopalpura","district":"arya mains"}}}
+D = {"address":{"address1":{"city":"kukas city","district":"jaipur"},
+                "address2":{"city":"gopalpura","district":"arya mains"}}}
 
-# print(D["address"]["address1"]['city'])
-# print(D["address"]["address1"]['district'])
-# print(D["address"]["address2"]['city'])
-# print(D["address"]["address2"]['district'])
+print(D["address"]["address1"]['city'])
+print(D["address"]["address1"]['district'])
+print(D["address"]["address2"]['city'])
+print(D["address"]["address2"]['district'])
 
 
 # Example
