41f3c8024183df01884f69a693923bbd183ebe86
mihul-katiwal <mihul9116640022@gmail.com>
2026-06-25 11:54:19 +0530

    Update T3_Conditional.py

--- T3_Conditional.py
+++ T3_Conditional.py
@@ -1,34 +1,34 @@
 # if
-# num1 = 10
-# num2 = 20
-# if num1 < num2:
-#     print(num2)
+num1 = 10
+num2 = 20
+if num1 < num2:
+    print(num2)
 
 # if else
-# num1 = int(input("Enter first number: "))
-# if num1 :
-#     print("number :-", num1)
-# else:
-#     print("zero", num1)
+num1 = int(input("Enter first number: "))
+if num1 :
+    print("number :-", num1)
+else:
+    print("zero", num1)
 
 # if elif else
-# age = int(input("Enter your age: "))
-# if age == 18:
-#     print("your age is:", age)
-# elif age > 18:
-#     print("you are above then:", age)
-# else:
-#     print("you are below then:", age)    
+age = int(input("Enter your age: "))
+if age == 18:
+    print("your age is:", age)
+elif age > 18:
+    print("you are above then:", age)
+else:
+    print("you are below then:", age)    
 
 
 # example
-# age = int(input("Enter your age: "))
-# if age == 18:
-#     print("aligible for vote")
-# elif age > 18:
-#     print("aligible for vote")
-# else:
-#     print("not aligible for vote")
+age = int(input("Enter your age: "))
+if age == 18:
+    print("aligible for vote")
+elif age > 18:
+    print("aligible for vote")
+else:
+    print("not aligible for vote")
 
 # example
 marks = int(input("Enter your marks: "))
