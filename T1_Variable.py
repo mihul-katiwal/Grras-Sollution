1e900f343c5f21bea8aa90842585abe4f93c757c
mihul-katiwal <mihul9116640022@gmail.com>
2026-06-25 11:51:03 +0530

    Update T1_Variable.py

--- T1_Variable.py
+++ T1_Variable.py
@@ -1,61 +1,61 @@
 # variable declaration
 
-# a = 10
-# print(a)
+a = 10
+print(a)
 
-# a = 10
-# b = 10
-# print(a , b)
-# print(id(a), id(b))
+a = 10
+b = 10
+print(a , b)
+print(id(a), id(b))
 
-# a = 20
-# b = a
-# a = 30
-# print(a, b)
-# print(id(a), id(b))
+a = 20
+b = a
+a = 30
+print(a, b)
+print(id(a), id(b))
 
 
-# a = input("Enter a number: ")
-# print(a)
-# print(type(a))
+a = input("Enter a number: ")
+print(a)
+print(type(a))
 
 
 
 # Integer
-# a = 10
-# print(a)
-# print(type(a))
+a = 10
+print(a)
+print(type(a))
 
 
 # Float
-# b = 10.10
-# print(b)
-# print(type(b))
+b = 10.10
+print(b)
+print(type(b))
 
 
 # String
-# c = "Hello World"
-# print(c)
-# print(type(c))
+c = "Hello World"
+print(c)
+print(type(c))
 
 
 # Boolean
-# d = True
-# print(d)
-# print(type(d))
+d = True
+print(d)
+print(type(d))
 
 
 # type Conversion & input/output
 
-# num = input("Enter first number: ")
-# print(num)
-# print(type(num))
-# print(int(num) + 10)    # both have type integer
-# print(num + str(10))    # both have type string
-# print(num * 10)
+num = input("Enter first number: ")
+print(num)
+print(type(num))
+print(int(num) + 10)    # both have type integer
+print(num + str(10))    # both have type string
+print(num * 10)
 
 
-# num = int(input("Enter a number: "))
-# print(num)
-# print(type(num))
-# print(num + 10)
+num = int(input("Enter a number: "))
+print(num)
+print(type(num))
+print(num + 10)
